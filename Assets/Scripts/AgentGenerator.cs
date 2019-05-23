721306bec4afe7fd26f5f0dd873eb7aafedeaf30
okadaakira <akira.okada@ga.aais-lab.org>
2019-05-23 18:42:25 +0900

    アニメーターの作成を行い，ステートマシンで状態を制御
    * Agentのprefabからinstanceを自動生成することができるようになった

---
+++ Assets/Scripts/AgentGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AgentGenerator : MonoBehaviour
+{
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            agentGen();
+    }
+
+    void agentGen()
+    {
+        var instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab, gameObject.transform);
+        PrefabUtility.UnpackPrefabInstance(instance, PrefabUnpackMode.OutermostRoot, InteractionMode.AutomatedAction);
+        instance.transform.parent = transform;
+    }
+}
---
+++ Assets/Scripts/HitogataAnimationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HitogataAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Speed", agent.velocity.magnitude);
+    }
+}
